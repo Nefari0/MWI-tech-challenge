1fbaafa4c1e636ddf2a755ac8971bb878cc0ed4d
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-15 09:05:59 -0600

    cleaned code

--- mwi-tech-challenge/src/App.js
+++ mwi-tech-challenge/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import MWIlogo from './assets/MWI-logo.png'
 import './App.css';
 import { useState,useEffect } from 'react';
 import Home from './Components/Home/Home/Home';
@@ -12,22 +12,24 @@ function App() {
     <div className="App">
       <header className="App-header">
       <header className="home-header" >
-        <h2 className={`header-left ${isContact ? true : 'header-left heading'}`} >Midwestern</h2>
-        <div className={`header-right ${isContact ? true : 'header-right contact'}`}  >{isContact === true ?<a onClick={() => setIsContact(!isContact)} >contact</a>:<div><a onClick={() => setIsContact(!isContact)} >home</a><Contact /></div>}</div>
+
+        <div className={`header-left ${isContact ? true : 'header-left heading'}`}>
+          <h2><img src={MWIlogo} /></h2>
+          {isContact === false ? <div className='heading-2-text'><h1 className="heading-1" >Heading One</h1>
+          <p className="home-text" style={{textAlign:'left',marginLeft:'40px'}}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic , remaining essentially unchanged
+          </p>
+          </div> : null}
+        </div>
+
+        <div className={`header-right ${isContact ? true : 'header-right contact'}`}  >
+          {isContact === true ?
+          <a onClick={() => setIsContact(!isContact)} >contact</a>
+          :
+          <div><a onClick={() => setIsContact(!isContact)} >home</a><Contact /></div>}
+        </div>
       </header>
         {isContact === true ? <Home /> : null}
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
