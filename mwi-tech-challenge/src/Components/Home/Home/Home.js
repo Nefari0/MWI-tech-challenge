936d2c8eab2acd67006a7eeeca82272c3806996f
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-15 09:10:12 -0600

    js code puzzle. cleaned code

--- mwi-tech-challenge/src/Components/Home/Home/Home.js
+++ mwi-tech-challenge/src/Components/Home/Home/Home.js
@@ -9,17 +9,17 @@ class Home extends Component {
 
         this.state = {
             data:[],
-            images:['https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Talkie.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Rabbit.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Shield.png']
-            // image1:
-            // image2:
-            // image3:
+            images:['https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Talkie.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Rabbit.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Shield.png'],
+            nameObj1:"Matt Johnson,Matt Johnson,Bart Paden,Ryan Doss,Jared Malcolm",
+            nameObj2:"Matt Johnson,Bart Paden,Bart Paden,Jordan Heigle,Jordan Heigle,Tyler Viles",
+            nameArray:[],
+            displayNames:false,
         }
     }
 
     componentDidMount = async () => {
         await axios.get(`https://api.mwi.dev/content/home`).then(res => {
         const { data } = res.data
-            // console.log('here is data',data[0].content)
             this.setState({
                 ...this.state,
                 data
@@ -28,30 +28,36 @@ class Home extends Component {
     }
 
     executeGetNames = () => {
-        // console.log('got names')
+        var { nameObj1,nameObj2} = this.state
+        var newArr = []
+        var returnDistinct = i => i.replace(/(\b\S.+\b)(?=.*\1)/g ,"").trim();
+        var nameArr = returnDistinct(nameObj1+','+nameObj2).split(',')
+        for (let x in nameArr) {
+            newArr.push({
+                name:nameArr[x],
+                id:x})
+        }
+        if(this.state.nameArray[0] === undefined){
+            this.setState({nameArray:newArr})
+        } else {alert('You already clicked this link')}
     }
 
     render() {
 
-        const { data,images } = this.state
+        const { data,images,nameArray } = this.state
 
         const mappedData = data.map(el => {
-            // console.log(el.content)
             return <Content key={el.id} content={el.content} image={images[el.id-1]} />
         })
 
+        const mappedNames = nameArray.map(el => {
+            console.log(el.name)
+            return <p style={{color:'white'}} key={el.id} name={el.name} >{el.name}</p>
+        })
+
         return(<div className="home">
-            {/* <header className="home-header" >
-                <h2>header</h2>
-                <a>contact</a>
-            </header> */}
             <section className="home-row-1">
-                {/* <img src='https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Rabbit.png' /> */}
-                {/* <img src={images[0]} /> */}
                 {mappedData}
-                {/* <Content /> */}
-                {/* <Content /> */}
-                {/* <Content /> */}
             </section>
 
             <section className="home-row-2" >
@@ -59,6 +65,7 @@ class Home extends Component {
                 <p className="home-text" style={{textAlign:'left',marginLeft:'40px'}}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic , remaining essentially unchanged<a onClick={() => this.executeGetNames()} style={{color:'#DEBF79'}}>  link</a>.
                 </p>
+            <div className="mapped-names">{mappedNames}</div>
             </section>
         </div>)
     }
