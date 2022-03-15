55b0f577e0df457d0dff7c81dd4a3eb7f0f86267
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-15 14:07:41 -0600

    <Contact/> is responsive

--- mwi-tech-challenge/src/App.js
+++ mwi-tech-challenge/src/App.js
@@ -28,6 +28,25 @@ function App() {
           :
           <div><a onClick={() => setIsContact(!isContact)} >home</a><Contact /></div>}
         </div>
+
+        {/* ---- header right / left mobile view ----- */}
+        <div className='mobile-header-left'>
+          <h2><img className='mwi-logo' src={MWIlogo} /></h2>
+        {isContact === false ? <div className='heading-2-text'><h1 className="heading-1" >Heading One</h1>
+          <p className="home-text" style={{textAlign:'left',marginLeft:'40px'}}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic , remaining essentially unchanged
+          </p>
+          </div> : null}
+          {/* <div className='mobile-contact-display' ><Contact /></div> */}
+          {isContact === false ? <Contact /> : null}
+        </div>
+
+        <div className='mobile-header-right'>
+          {isContact === true ? <a onClick={() => setIsContact(!isContact)}>contact</a> : <a onClick={() => setIsContact(!isContact)}>home</a>}
+        </div>
+
+
+
       </header>
         {isContact === true ? <Home /> : null}
       </header>
