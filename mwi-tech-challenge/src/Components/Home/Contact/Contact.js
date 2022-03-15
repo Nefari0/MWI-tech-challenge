5f9cf554068178aa3fbd9f75f5ec7e166e503b8d
Nefari0 <chrisdemontigny@yahoo.com>
2022-03-15 14:09:28 -0600

    <Contact/> is responsive. cleaned code

--- mwi-tech-challenge/src/Components/Home/Contact/Contact.js
+++ mwi-tech-challenge/src/Components/Home/Contact/Contact.js
@@ -29,7 +29,6 @@ class Contact extends Component {
     }
 
     sendInfo = () => {
-        // console.log('hit "sendInfo"')
         const { firstName,lastName,title,email,message } = this.state
         axios.post(`/api/contacts/new`,{firstName,lastName,title,email,message})
     }
@@ -38,11 +37,24 @@ class Contact extends Component {
 
         return(<div className='contact-page'>
             <h2 className='heading-2' style={{marginLeft:'22px'}} >Heading Two</h2>
-            <div className='small-input-row' ><input onChange={(e) => this.inputHandler('firstName',e.target.value)} className='small-input' placeholder='First Name'/><input onChange={(e) => this.inputHandler('lastName',e.target.value)}className='small-input' placeholder='Last Name' /></div>
-            <div className='small-input-row' ><input onChange={(e) => this.inputHandler('title',e.target.value)} className='small-input' placeholder='Title'/><input onChange={(e) => this.inputHandler('email',e.target.value)} className='small-input' placeholder='Email'/></div>
-            {/* <div className='small-input-row' ><input className='large-input' /></div> */}
-            <div className='small-input-row'><textarea onChange={(e) => this.inputHandler('message',e.target.value)} className='large-input' placeholder='Message' ></textarea></div>
-            <div onClick={() => this.sendInfo()} className='submit-button'><p className='content-box-text' style={{color:'#fff'}}>Submit</p></div>
+
+            <div className='small-input-row' >
+                <input onChange={(e) => this.inputHandler('firstName',e.target.value)} className='small-input' placeholder='First Name'/>
+                <input onChange={(e) => this.inputHandler('lastName',e.target.value)}className='small-input' placeholder='Last Name' />
+            </div>
+
+            <div className='small-input-row' >
+                <input onChange={(e) => this.inputHandler('title',e.target.value)} className='small-input' placeholder='Title'/>
+                <input onChange={(e) => this.inputHandler('email',e.target.value)} className='small-input' placeholder='Email'/>
+            </div>
+
+            <div className='small-input-row'>
+                <textarea onChange={(e) => this.inputHandler('message',e.target.value)} className='large-input' placeholder='Message' ></textarea>
+            </div>
+
+            <div onClick={() => this.sendInfo()} className='submit-button'>
+                <p className='content-box-text' style={{color:'#fff'}}>Submit</p>
+            </div>
         </div>)
     }
 }
