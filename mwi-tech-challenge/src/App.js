12a73ed015958bd2c0af11a75eae9cf3153dc036
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-22 11:43:11 -0600

    Rebuilding

--- mwi-tech-challenge/src/App.js
+++ mwi-tech-challenge/src/App.js
@@ -2,7 +2,9 @@ import MWIlogo from './assets/MWI-logo.png'
 import './App.css';
 import { useState,useEffect } from 'react';
 import Home from './Components/Home/Home';
-import Contact from './Components/Home/Contact/Contact';
+import Contact from './Components/Contact/Contact';
+import NavBar from './Components/Nav/nav.component'
+// import Contact from '../../'
 
 function App() {
 
@@ -10,45 +12,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-      <header className="home-header" >
+      <NavBar/>
+      <Home />
 
-        <div className={`header-left ${isContact ? true : 'header-left heading'}`}>
-          <h2><img className='mwi-logo' src={MWIlogo} /></h2>
-          {isContact === false ? <div className='heading-2-text'><h1 className="heading-1" >Heading One</h1>
-          <p className="home-text" style={{textAlign:'left',marginLeft:'40px'}}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic , remaining essentially unchanged
-          </p>
-          </div> : null}
-        </div>
-
-        <div className={`header-right ${isContact ? true : 'header-right contact'}`}  >
-          {isContact === true ?
-          <a onClick={() => setIsContact(!isContact)} >contact</a>
-          :
-          <div><a onClick={() => setIsContact(!isContact)} >home</a><Contact /></div>}
-        </div>
-
-        {/* ---- header right / left mobile view ----- */}
-        <div className='mobile-header-left'>
-          <h2><img className='mwi-logo' src={MWIlogo} /></h2>
-        {isContact === false ? <div className='heading-2-text'><h1 className="heading-1" >Heading One</h1>
-          <p className="home-text" style={{textAlign:'left',marginLeft:'40px'}}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic , remaining essentially unchanged
-          </p>
-          </div> : null}
-          {isContact === false ? <Contact /> : null}
-        </div>
-
-        <div className='mobile-header-right'>
-          {isContact === true ? <a onClick={() => setIsContact(!isContact)}>contact</a> : <a onClick={() => setIsContact(!isContact)}>home</a>}
-        </div>
-
-
-
-      </header>
-        {isContact === true ? <Home /> : null}
-      </header>
     </div>
   );
 }
