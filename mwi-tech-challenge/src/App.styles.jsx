acfb24715ba79420cc4fdeb956bad325afc80122
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-24 05:38:28 -0600

    Transitioning to styled-components

---
+++ mwi-tech-challenge/src/App.styles.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.main`
+    text-align: center;
+    background-color: #222222;
+    min-width: 400px;
+    /* max-width: 1200px; */
+    margin:auto;
+    /* position: relativ; */
+    /* padding-left:200px; */
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+`
