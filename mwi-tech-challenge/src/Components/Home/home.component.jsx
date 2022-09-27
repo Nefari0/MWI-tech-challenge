bbeb033f30019d7d65af25e79e70814ff0e1de69
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-27 13:12:34 -0600

    Converted to functional component

--- mwi-tech-challenge/src/Components/Home/home.component.jsx
+++ mwi-tech-challenge/src/Components/Home/home.component.jsx
@@ -1,63 +1,66 @@
 import axios from "axios";
-import { Component } from 'react'
+import {useEffect,useState} from 'react'
 import Content from "./HomeCard/content.component.";
 import { HeadingOne } from "../HeadingOne/heading-one.component";
 import { HomePage } from "./home.styles";
 import { HomeContent } from "./home.styles";
+import { Notice } from "../Styles/Notice/notice.component";
 
-class Home extends Component {
-    constructor() {
-        super()
+const Home = () => {
 
-        this.state = {
-            data:[],
-            images:['https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Talkie.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Rabbit.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Shield.png'],
-            nameObj1:"Matt Johnson,Matt Johnson,Bart Paden,Ryan Doss,Jared Malcolm",
-            nameObj2:"Matt Johnson,Bart Paden,Bart Paden,Jordan Heigle,Jordan Heigle,Tyler Viles",
-            nameArray:[],
-            displayNames:false,
-        }
-    }
+    const [state,setState] = useState({
+        data:[],
+        images:['https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Talkie.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Rabbit.png','https://raw.githubusercontent.com/Midwestern-Interactive/tech-challenge/master/assets/Shield.png'],
+        nameObj1:"Matt Johnson,Matt Johnson,Bart Paden,Ryan Doss,Jared Malcolm",
+        nameObj2:"Matt Johnson,Bart Paden,Bart Paden,Jordan Heigle,Jordan Heigle,Tyler Viles",
+        nameArray:[],
+        notice:null
+    })
 
-    componentDidMount = () => {
+    var { nameObj1,nameObj2,data,images,nameArray,notice} = state
+
+    useEffect(() => {getItems()},[])
+
+    const getItems = () => {
         axios.get(`https://api.mwi.dev/content/home`).then(res => {
-        const { data } = res.data
-            this.setState({
-                ...this.state,
+            const { data } = res.data
+            setState({
+                ...state,
                 data
             })
         })
     }
 
-    executeGetNames = () => {
-        var { nameObj1,nameObj2} = this.state
-        var newArr = []
-        var returnDistinct = i => i.replace(/(\b\S.+\b)(?=.*\1)/g ,"").trim();
-        var nameArr = returnDistinct(nameObj1+','+nameObj2).split(',')
-        for (let x in nameArr) {
-            newArr.push({
-                name:nameArr[x],
-                id:x})
-        }
-        if(this.state.nameArray[0] === undefined){
-            this.setState({nameArray:newArr})
-        } else {alert('You already clicked this link')}
+    const closeNotice = () => { // -- Function for closing Notice component
+        setState({...state,notice:''})
     }
 
-    render() {
+    const executeGetNames = () => {
+        var returnDistinct = i => i.replace(/(\b\S.+\b)(?=.*\1)/g ,"").trim();
+        var nameArr = returnDistinct(nameObj1+','+nameObj2).split(',')
 
-        const { data,images,nameArray } = this.state
+        if(nameArray[0] === undefined){
+            setState({...state,nameArray:nameArr})
+        } else {
+            setState({...state,notice:'You already clicked this link'})
+        }
+    }
 
-        const mappedData = data.map(el => {
-            return <Content key={el.id} content={el.content} image={images[el.id-1]} />
-        })
+    const mappedData = data.map(el => {
+        return <Content key={el.id} content={el.content} image={images[el.id-1]} />
+    })
 
-        const mappedNames = nameArray.map(el => {
-            return <p style={{color:'white'}} key={el.id} name={el.name} >{el.name}</p>
-        })
+    const mappedNames = nameArray.map((el,i) => {
+        return <p style={{color:'white'}} key={i}>{el}</p>
+    })
 
-        return (
-            <HomePage>
+    return (
+        <HomePage>
+
+            {notice && <Notice
+                text={notice}
+                clearNotice={closeNotice}
+            />}
 
             <HomeContent>
                 {mappedData}
@@ -67,13 +70,15 @@ class Home extends Component {
                 Remove the duplicates in 2 Javascript arrays,
                 add the results to an array and output the list of distinct
                 names in an unordered list below this paragraph when{' '}
-                <a onClick={this.executeGetNames}>this link</a> is clicked. If the
+                <a onClick={executeGetNames}>this link</a> is clicked. If the
                 operation has been completed already, notify the user that this
                 has already been done.
             </HeadingOne>
 
-        </HomePage>)
-    }
+            {nameArray.length > 1 && mappedNames}
+
+        </HomePage>
+    )
 }
 
 export default Home
