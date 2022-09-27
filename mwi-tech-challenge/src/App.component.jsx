95682b05907052ad516bdd999a036f7b3098652f
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-27 11:37:27 -0600

    Rebuilding

---
+++ mwi-tech-challenge/src/App.component.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/contact.component';
+import NavBar from './Components/Nav/nav.component';
+import { AppContainer } from './App.styles';
+
+function App() {
+
+  const [isContact,setIsContact] = useState(true)
+
+  return (
+
+    <AppContainer>
+      <NavBar
+        isContact={isContact}
+        setIsContact={setIsContact}
+      />
+      {isContact ?
+        <Home/>
+        :
+        <Contact/>
+      }
+    </AppContainer>
+  );
+};
+
+export default App;
