a612b9940e682cadac5c1d06d1613b3ddd3f768a
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-26 21:59:32 -0600

    Rebuilding

--- mwi-tech-challenge/src/App.js
+++ mwi-tech-challenge/src/App.js
@@ -1,21 +1,28 @@
 import MWIlogo from './assets/MWI-logo.png'
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import Home from './Components/Home/Home';
 import Contact from './Components/Contact/Contact';
 import NavBar from './Components/Nav/nav.component'
-// import Contact from '../../'
+import { AppContainer } from './App.styles';
 
 function App() {
 
   const [isContact,setIsContact] = useState(true)
 
   return (
-    <div className="App">
-      <NavBar/>
-      <Home />
 
-    </div>
+    <AppContainer>
+      <NavBar
+        isContact={isContact}
+        setIsContact={setIsContact}
+      />
+      {isContact ?
+        <Home/>
+        :
+        <Contact/>
+      }
+    </AppContainer>
   );
 }
 
