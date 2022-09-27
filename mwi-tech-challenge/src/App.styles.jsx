72773a8e94aa7cf6f55a7b4809d5230708468a3e
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-27 10:06:48 -0600

    Cleanup

--- mwi-tech-challenge/src/App.styles.jsx
+++ mwi-tech-challenge/src/App.styles.jsx
@@ -1,11 +1,8 @@
 import styled from "styled-components";
-import { colors } from "./Components/Styles/colors";
 import { device, size } from "./Components/Styles/queries";
-const { darkGray } = colors
 
 export const AppContainer = styled.main`
     text-align: center;
-    background-color: ${darkGray};
     width:${size.desktop};
     margin:auto;
     overflow:scroll;
