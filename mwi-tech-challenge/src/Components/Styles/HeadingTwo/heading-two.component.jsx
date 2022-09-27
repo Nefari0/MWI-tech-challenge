f39614e3cd656c3584bb6aaf8148e748849d3130
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-27 11:29:33 -0600

    Base heading two component

---
+++ mwi-tech-challenge/src/Components/Styles/HeadingTwo/heading-two.component.jsx
@@ -0,0 +1,7 @@
+import { H2 } from "./heading-two.styles"
+
+export const HeadingTwo = ({children}) => {
+    return (
+        <H2>{children}</H2>
+    )
+}
---
+++ mwi-tech-challenge/src/Components/Styles/HeadingTwo/heading-two.styles.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const H2 = styled.h2`
+    width:100%;
+    text-align:left;
+    font-size:25px;
+`
