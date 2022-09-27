9819bf655d7e1334a4b4e8811e3560384ae5338e
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-27 10:22:40 -0600

    Cleanup

--- mwi-tech-challenge/src/Components/Contact/Form/FormInput.styles.jsx
+++ mwi-tech-challenge/src/Components/Contact/Form/FormInput.styles.jsx
@@ -5,85 +5,71 @@ const { query } = device
 const { lightGray,darkGray } = colors
 
 const shrinkLabelStyles = css`
-  top: -10px;
-  font-size: 12px;
+    top: -10px;
+    font-size: 12px;
 `;
 
 export const FormInputLabel = styled.label`
-  color: ${darkGray};
-  font-size: 16px;
-  font-weight: normal;
-  position: absolute;
-  pointer-events: none;
-  left: 5px;
-  top:20px;
-  transition: 300ms ease all;
+    color: ${darkGray};
+    font-size: 16px;
+    font-weight: normal;
+    position: absolute;
+    pointer-events: none;
+    left: 5px;
+    top:20px;
+    transition: 300ms ease all;
 
-  ${({ shrink }) => shrink && shrinkLabelStyles};
+    ${({ shrink }) => shrink && shrinkLabelStyles};
+
 `;
 
 export const Input = styled.input`
-  background: none;
-  background-color: ${lightGray};
-  color: ${darkGray};
-  font-size: 18px;
-  padding: 10px 10px 10px 5px;
-  display: block;
-  width: 90%;
-  height:30px;
-  border: none;
-  border-radius: 0;
+    background: none;
+    background-color: ${lightGray};
+    color: ${darkGray};
+    font-size: 18px;
+    padding: 10px 10px 10px 5px;
+    display: block;
+    width: 90%;
+    height:30px;
+    border: none;
+    border-radius: 0;
+    margin: 10px 0;
 
-//   margin: 25px 0;
-  margin: 10px 0;
-  &:focus {
-    outline: none;
-  }
-  &:focus ~ ${FormInputLabel} {
-    ${shrinkLabelStyles};
-  }
+    &:focus {
+      outline: none;
+    }
+
+    &:focus ~ ${FormInputLabel} {
+      ${shrinkLabelStyles};
+    }
 `;
 
 export const Message = styled(Input)`
-  width:90%;
-  height:100px;
-`
+    width:90%;
+    height:100px;
+`;
 
 export const Group = styled.div`
-
-width:50%;
-height:0px;
-
-margin:auto;
-margin-top:-10px;
-
-  position: relative;
-
-
+    width:50%;
+    height:0px;
+    margin:auto;
+    margin-top:-10px;
+    position: relative;
 `;
 
 export const Form = styled.form`
     position: absolute;
-    // position:relative;
-    // margin:auto;
-    
     margin:auto;
-  height: 500px;
-  padding-top:200px;
-  padding-bottom:200px;
-  width: 600px;
-  background-color: #fff;
-
-    // bottom: 100px;
+    height: 500px;
+    padding-top:200px;
+    padding-bottom:200px;
+    width: 600px;
+    background-color: #fff;
     right: 0px;
-    // height:300px;
-    // width: 650px;
     display: flex;
-    // flex-direction: ro;
     flex-wrap: wrap;
-    // justify-content: space-around;
-    // align-items: flex-start;
-    // align-content: flex-end;
+    align-items: center;
 
     @media ${query} {
         position: relative;
