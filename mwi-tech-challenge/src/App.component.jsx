76719beaf137fb7ade4f963d26a8f404514e0f52
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-28 11:00:15 -0600

    Styling adjustemnts

--- mwi-tech-challenge/src/App.component.jsx
+++ mwi-tech-challenge/src/App.component.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react';
 import Home from './Components/Home/home.component';
 import Contact from './Components/Contact/contact.component';
 import NavBar from './Components/Nav/nav.component';
-import { HeadingOne } from './Components/HeadingOne/heading-one.component';
-import { AppContainer } from './App.styles';
+import { AppContainer,ViewPort } from './App.styles';
 
 function App() {
 
@@ -12,34 +11,24 @@ function App() {
   return (
 
     <AppContainer>
-      <NavBar
-        isContact={isContact}
-        setIsContact={setIsContact}
-      />
-      {/* {!isContact && <Home/>} */}
-      {/* {isContact && <Contact />} */}
-            {/* <HeadingOne title={'Heading One'}>
-                Remove the duplicates in 2 Javascript arrays,
-                add the results to an array and output the list of distinct
-                names in an unordered list below this paragraph when{' '}
-                <a >this link</a> is clicked. If the
-                operation has been completed already, notify the user that this
-                has already been done.
-            </HeadingOne> */}
-
-            
-     
-      {!isContact ?
-        <Home
+      <ViewPort>
+        <NavBar
           isContact={isContact}
           setIsContact={setIsContact}
-        />
-        :
-        <Contact
-          isContact={isContact}
-          setIsContact={setIsContact}
-        />
-      }
+        />            
+      
+        {!isContact ?
+          <Home
+            isContact={isContact}
+            setIsContact={setIsContact}
+          />
+          :
+          <Contact
+            isContact={isContact}
+            setIsContact={setIsContact}
+          />
+        }
+      </ViewPort>
     </AppContainer>
   );
 };
--- mwi-tech-challenge/src/App.styles.jsx
+++ mwi-tech-challenge/src/App.styles.jsx
@@ -1,22 +1,30 @@
 import styled from "styled-components";
 import { device, size } from "./Components/Styles/queries";
 import { colors } from './Components/Styles/colors'
-const { darkGray } = colors
+const { darkGray,black } = colors
 const { mobileW,desktopW } = size
 
 export const AppContainer = styled.main`
     text-align: center;
-    width:${desktopW};
-    height:100vh;
-    background-color:${darkGray};
-    margin:auto;
-    overflow:scroll;
+    background-color:${black};
+    min-height:1000px;
+    height:auto;
     -ms-overflow-style: none;  /* IE and Edge */
     scrollbar-width: none;  /* Firefox */
+
+    a {cursor: pointer;}
+`
+
+export const ViewPort = styled.section`
+    positon:absolute;
+    width:${desktopW};
+    min-height:100vh;
+    background-color:${darkGray};
+    margin:auto;
     display: flex;
     flex-direction: column;
-
-    @media ${device.query} {
-        width:${mobileW}
-    }
+    overflow:hidden;
+    z-index:10000;
+    
+    @media ${device.query} {width:${mobileW};}
 `
