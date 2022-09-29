885a1d373cbb4e377a043f2b60156f0cf04cde5f
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-29 16:45:14 -0600

    Added notice component, which will be used here and controlled through redux throughout the app

--- mwi-tech-challenge/src/App.component.jsx
+++ mwi-tech-challenge/src/App.component.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 import {setSpinner} from './dux/isLoadingReducer';
+import { clearMessage } from './dux/noticeReducer';
 import { connect } from 'react-redux'
 import Home from './Components/Home/home.component';
 import Contact from './Components/Contact/contact.component';
 import NavBar from './Components/Nav/nav.component';
 import { AppContainer } from './App.styles';
 import Spinner from './Components/Spinner/spinner.component';
+import { Notice } from './Components/Notice/notice.component';
 
 function App(props) {
 
   const [isContact,setIsContact] = useState(false)
   const { isLoading } = props.isLoading
+  const { notice } = props.notice
 
   return (
 
     <AppContainer>
       <section>
+        {notice && <Notice text={notice} clearNotice={props.clearMessage} />}
 
         {isLoading && <Spinner/>}
         
@@ -44,4 +48,4 @@ function mapStateToProps(reduxState){
   return reduxState
 }
 
-export default connect(mapStateToProps, { setSpinner })(App)
+export default connect(mapStateToProps, { setSpinner,clearMessage })(App)
