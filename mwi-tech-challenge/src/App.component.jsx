7d747bf448dae77679d2e89c8e96de2d0bc75ecc
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-29 15:25:25 -0600

    Redux added. Using loading screen globally

--- mwi-tech-challenge/src/App.component.jsx
+++ mwi-tech-challenge/src/App.component.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
+import isLoadingReducer from './dux/isLoadingReducer';
+import { connect } from 'react-redux'
 import Home from './Components/Home/home.component';
 import Contact from './Components/Contact/contact.component';
 import NavBar from './Components/Nav/nav.component';
 import { AppContainer } from './App.styles';
+import Spinner from './Components/Spinner/spinner.component';
 
-function App() {
+function App(props) {
 
   const [isContact,setIsContact] = useState(false)
+  const { isLoading } = props.isLoading
 
   return (
 
     <AppContainer>
       <section>
+
+        {isLoading && <Spinner/>}
+        
         <NavBar
           isContact={isContact}
           setIsContact={setIsContact}
@@ -33,4 +40,8 @@ function App() {
   );
 };
 
-export default App;
+function mapStateToProps(reduxState){
+  return reduxState
+}
+
+export default connect(mapStateToProps, { isLoadingReducer })(App)
