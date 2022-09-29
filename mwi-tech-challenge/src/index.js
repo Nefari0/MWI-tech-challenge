2fb39e4de1a193d098d1aa7fec82989ca3be510c
Nefari0 <chrisdemontigny@yahoo.com>
2022-09-29 15:28:27 -0600

    Redux added to project

--- mwi-tech-challenge/src/index.js
+++ mwi-tech-challenge/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.component';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './dux/store';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
 const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
